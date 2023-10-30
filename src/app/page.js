a46d712451cdf76ff7b65095639044fce8e42080
vladihka <vladihka58@gmail.com>
2023-10-30 17:39:20 +0100

    Add body section

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import HomeHeader from "@/components/HomeHeader";
+import HomeSearch from "@/components/HomeSearch";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,6 +9,14 @@ export default function Home() {
     return (
         <>
             <HomeHeader></HomeHeader>
+
+            <div className="flex flex-col items-center mt-24">
+                <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1920px-Google_2015_logo.svg.png"
+                       alt="asdas"
+                       width="300"
+                       height="100"></Image>
+                <HomeSearch></HomeSearch>
+            </div>
         </>
     )
 }
